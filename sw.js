2dbd8ce0b5998014570e1a059a453714a30eec3d
zluo7467-debug <zluo7467@gmail.com>
2026-09-19 19:30:53 +0800

    发布栈川行网页

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'zhanchuanxing-v1'
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './app-icon.svg']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)))
+})
